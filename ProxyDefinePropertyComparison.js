73339dc36e0be77a5c5d48b75cc338426588f3a5
apolodu <dudueasy@126.com>
2023-08-02 22:20:16 +0800

    feat: update note for Proxy

--- ProxyDefinePropertyComparison.js
+++ ProxyDefinePropertyComparison.js
@@ -1,4 +1,4 @@
-
+// example1: 访问属性示例:
 {
     const xiaowang = {
         love: "我喜欢你，我想和你睡觉"
@@ -12,7 +12,7 @@
     console.log(sendToMyLove(xiaowang));
 }
 
-// example 2: 使用 Object.defineProperty, "劫持属性访问"
+// example 2: 使用 Object.defineProperty "劫持属性访问"
 {
     const xiaowang = {
         loveLetter: "我喜欢你，我想和你睡觉"
@@ -31,7 +31,7 @@
     console.log(sendToMyLove(xiaowang));
 }
 
-// example 3 使用Proxy "劫持" 属性访问
+// example 3: 使用Proxy "劫持" 属性访问
 {
     const xiaowang = {
         loveLetter: "我喜欢你，我想和你睡觉"
@@ -54,8 +54,7 @@
     console.log(sendToMyLove(proxy));
 }
 
-// 示例: 使用 Object.defineProperty 监听属性变化 
-
+// example: 使用 Object.defineProperty 监听属性变化
 {
     console.log('开始 Object.defineProperty 示例')
     function observe(obj, callback) {
@@ -80,7 +79,6 @@
         return newObj;
     }
 
-
     const obj = observe(
         {
             name: "子君",
@@ -95,35 +93,38 @@
     obj.gender = '女'
 }
 
-
+// example5: 使用 Proxy 监听对象
 {
-
     console.log('开始 Proxy 实例')
 
     function observe(obj, callback) {
         // 必须提供一个 obj 作为中介 (newObj), 否则在 setter 中对同一个对象进行赋值, 就会造成递归.
-        let newObj = new Proxy({}, {
-            get: (target, property) => {
+        let newObj = new Proxy(obj, {
+            // receiver 是 new 出来的 proxy 对象, 也就是 newObj 自己
+            get: (target, property, receiver) => {
+                console.log(`target`, target)
+                console.log(`receiver`, receiver)
                 return target[property]
             },
 
-            set: (target, property, value) => {
+            set: (target, property, value, receiver) => {
+                console.log(`target`, target)
+                console.log(`receiver`, receiver)
                 console.log(`属性[${property}]的值被修改为[${value}]`);
                 target[property] = value;
             }
-
         })
-
 
         return newObj;
     }
 
+    const targetObj = {
+        name: "子君",
+        gender: "男"
+    };
 
     const obj = observe(
-        {
-            name: "子君",
-            gender: "男"
-        },
+        targetObj,
         (key, value) => {
             console.log(`属性[${key}]的值被修改为[${value}]`);
         }
@@ -132,5 +133,29 @@
     obj.name = "妹纸";
     obj.gender = '女'
 
+    console.log(`obj`, obj)
+    console.log(`targetObj`, targetObj)
 }
+
+// 在 Proxy 内部使用 Reflect.get Reflect.set 来实现反射
+{
+    const obj = { }
+
+    const proxy = new Proxy(obj, {
+        get(target, p, receiver) {
+            return Reflect.get(target, p, receiver)
+        },
+        set(target, p, value, receiver) {
+            return Reflect.set(target, p, value, receiver)
+        }
+    })
+
+    proxy.name = '123'
+    console.log(`proxy.name`, proxy.name)
+
+    console.log(`obj`, obj)
+    console.log(`proxy`, proxy)
+}
+
+
 
